7940be9ddf4ee5c3846d334e95d7d65f76d9ba5a
gioiast <gioia.stroffolini@mail.polimi.it>
2022-09-27 12:07:48 +0200

    Completed artwork

--- sketch.js
+++ sketch.js
@@ -4,10 +4,57 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background('#FAEBD7');
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+
+let x = random (0, width) // imposto una variabile affinché le coordinate della posizione delle geometrie sia randomica
+let y = random (0, height)
+if(random()<0.7) // imposto un framerate diverso per la comparsa delle forme, con due opzioni valide
+{ frameRate(500)
+} else {
+  frameRate(100)
+}
+
+// ora procedo a definire le forme, tre cerchi di colori diversi che possono creare ulteriori colori diversi per sovrapposizione, e dei quadrati con solo stroke nero
+push()
+scale (random(0, 5)) // do un attributo randomico anche allo scaling per ottenere cerchi di grandezze diverse
+blendMode(SCREEN)
+fill ("lime")
+noStroke()
+ellipse(x,y,15)
+pop()
+
+push()
+scale (random(0, 5))
+blendMode(SCREEN)
+fill ("coral")
+noStroke()
+ellipse(x,y,10)
+pop()
+
+push()
+scale (random(0, 10))
+blendMode(SCREEN)
+fill ("blue")
+noStroke()
+ellipse(x,y,10)
+pop()
+
+push()
+rotate(45*frameCount);
+noFill();
+stroke('black');
+strokeWeight(0.5);
+rect(x,y,5)
+pop()
+
+// imposto un if statement per fare in modo che il disegno finisca dopo un certo framecount 
+if (frameCount == 800) {
+
+  noLoop();
+}
+
 }
